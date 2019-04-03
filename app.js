2b6704d0791851eded63d6ef19be7af98618ef7d
Ben Harrison <bvdhest1998@gmail.com>
2019-04-02 18:16:35 -0700

    -last videos added

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname));
 
+app.get('/Galaga-GET', function(req, res){
+	res.json({url: "https://www.youtube.com/embed/MyIPBufxl30?&autoplay=1"});
+});
 
 app.get('/Frogger-GET', function(req, res){
 	res.json({url: "https://www.youtube.com/embed/WNrz9_Fe-Us"});
@@ -17,6 +20,16 @@ app.get('/Frogger-GET', function(req, res){
 app.get('/Pac-Man-GET', function(req, res){
 	res.json({url: "https://www.youtube.com/embed/dScq4P5gn4A"});
 });
+
+app.get('/DonkeyKong-GET', function(req, res){
+	res.json({url: "https://www.youtube.com/embed/Pp2aMs38ERY"});
+});
+
+app.get('/ExciteBike-GET', function(req, res){
+	res.json({url: "https://www.youtube.com/embed/ajb4AeyzDGI"});
+});
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
